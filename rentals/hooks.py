e648e9a185d089d7724e128681e3fa06b3b80a57
Kanagarla Suryakundan <kanagarla@Kanagarlas-MacBook-Air.local>
2026-06-27 13:47:28 +0530

    Add workflow fixture for ride booking approval

--- rentals/hooks.py
+++ rentals/hooks.py
@@ -9,7 +9,7 @@ app_license = "mit"
 # ------------------
 
 # required_apps = []
-fixtures = ["Vehicle Type"]
+fixtures = ["Vehicle Type","Workflow"]
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
